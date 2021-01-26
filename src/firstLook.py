2743c627ba5d6617e3f606dd5787a84d4b99602c
Dennis Heuermann <dennis@heuermann.de>
2021-01-26 18:54:40 +0100

    add comments

--- src/firstLook.py
+++ src/firstLook.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from pathlib import Path
+
+#resolve path to csv file from anywhere
 filename = "ks-projects-201801.csv"
 path = Path(__file__).resolve().parents[1]
 file = Path(path, filename)
+
 df = pd.read_csv(file)
 print(df.describe())
