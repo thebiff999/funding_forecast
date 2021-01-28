b598d0efedbec1831e7b9df0aa80cf2bbbf42ef9
prichi99 <77013132+prichi99@users.noreply.github.com>
2021-01-28 23:51:28 +0100

    Verbesserung real

--- src/percentage_reached_column.py
+++ src/percentage_reached_column.py
@@ -1,4 +1,4 @@
-print(df.pledged.tail( n = 5) )                             #data inspection
+print(df.usd_pledged_real.tail( n = 5) )                                           #data inspection
 print(df.goal.tail(n = 5))
-df['percentage_reached'] = df['pledged']/df['goal']         #new column with percentage pledged
-print(df['percentage_reached'].tail(n = 5))                 #data inspection
+df['percentage_reached_real'] = df['usd_pledged_real']/df['usd_goal_real']         #new column with
+print(df['percentage_reached_real'].tail(n = 5))                                   #data inspection
