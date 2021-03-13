46e5efcddf81b315f29fdaf9fe67217fe1c14d51
prichi99 <77013132+prichi99@users.noreply.github.com>
2021-03-13 16:03:14 +0100

    Delete Scatterplot_duration_percentage-goal-reached.py

--- src/Scatterplot_duration_percentage-goal-reached.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-percentage_reached_real = df['percentage_reached_real']
-duration_days = df['duration_days']
-
-plt.scatter(duration_days, percentage_reached_real)
-plt.xlabel('Duration (in days)')
-plt.ylabel('Percentage of the Goal reached')
-plt.show()
