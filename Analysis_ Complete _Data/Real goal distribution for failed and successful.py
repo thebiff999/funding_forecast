a6621c7c9ca38744e084f965927b905c1016da36
prichi99 <77013132+prichi99@users.noreply.github.com>
2021-03-18 18:36:10 +0100

    Update Real goal distribution for failed and successful.py
    Axis adjustion for better visibility

--- Analysis_ Complete _Data/Real goal distribution for failed and successful.py
+++ Analysis_ Complete _Data/Real goal distribution for failed and successful.py
@@ -2,36 +2,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#Boxplots for Goal (Real US$)
 plt.subplot(1,2,1)
 sns.boxplot(x=df["state"],
             y=df["usd_goal_real"])
-plt.ylabel('Goal (US$)')
+plt.ylabel('Goal (Real US$)')
+plt.ylim(-5000, 50000)
+plt.xlim(None, None)
 
+#Violinplots for Goal (Real US$)
 plt.subplot(1,2,2)
 sns.violinplot(x=df["state"],
                 y=df["usd_goal_real"])
 plt.ylabel('Goal (US$)')
+plt.ylim(-5000, 100000)
+plt.xlim(None, None)
 plt.show()
 
 
-print(df['state'].unique())
-
-dffailed = df[df['state'].isin(['failed'])]
+df_failed = df[df['state'].isin(['failed'])]
 print('Maximum of real goal failed projects')
-print(dffailed['usd_goal_real'].max())
+print(df_failed['usd_goal_real'].max())
 print('Minimum of real goal failed projects')
-print(dffailed['usd_goal_real'].min())
+print(df_failed['usd_goal_real'].min())
 print('Median of real goal failed projects')
-print(np.median(dffailed['usd_goal_real']))
+print(np.median(df_failed['usd_goal_real']))
 
-dfsuccessful = df[df['state'].isin(['successful'])]
-print(dfsuccessful['state'].unique())
+df_successful = df[df['state'].isin(['successful'])]
+print(df_successful['state'].unique())
 print('Maximum of  goal successful projects')
-print(dfsuccessful['usd_goal_real'].max())
+print(df_successful['usd_goal_real'].max())
 print('Minimum of goal successful projects')
-print(dfsuccessful['usd_goal_real'].min())
+print(df_successful['usd_goal_real'].min())
 print('Median of real goal failed projects')
-print(np.median(dfsuccessful['usd_goal_real']))
-
+print(np.median(df_successful['usd_goal_real']))
 
 #Maximum and Median of real goal is much larger for failed Projects
