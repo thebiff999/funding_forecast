1027e0eb5db83c6da72b1baf0682a5b3a87a6f3a
prichi99 <77013132+prichi99@users.noreply.github.com>
2021-03-18 19:20:00 +0100

    Update Real goal distribution for failed and successful.py

--- Analysis_ Complete _Data/Real goal distribution for failed and successful.py
+++ Analysis_ Complete _Data/Real goal distribution for failed and successful.py
@@ -19,22 +19,34 @@ plt.ylim(-5000, 100000)
 plt.xlim(None, None)
 plt.show()
 
-
+#more Information of the Data
 df_failed = df[df['state'].isin(['failed'])]
+
 print('Maximum of real goal failed projects')
 print(df_failed['usd_goal_real'].max())
+
 print('Minimum of real goal failed projects')
 print(df_failed['usd_goal_real'].min())
+
 print('Median of real goal failed projects')
 print(np.median(df_failed['usd_goal_real']))
 
+
 df_successful = df[df['state'].isin(['successful'])]
-print(df_successful['state'].unique())
+
 print('Maximum of  goal successful projects')
 print(df_successful['usd_goal_real'].max())
+
 print('Minimum of goal successful projects')
 print(df_successful['usd_goal_real'].min())
+
 print('Median of real goal failed projects')
 print(np.median(df_successful['usd_goal_real']))
 
+
+#boxplot shows heavy outliers for projects with failed state
+#larger range of goal for failed projects
 #Maximum and Median of real goal is much larger for failed Projects
+#Most of the successful projects have a goal < 10.000
+
+#high goals could ctrontribute to the failing of a project
